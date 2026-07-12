1f98cd975dc4a621cd965ee86ca6a27cb3debcc9
Andrija Vranic <ydrea.wrld@gmail.com>
2026-07-12 20:15:04 +0200

    Add native Cumquat version contract

--- modules/cumquat-native/specs/NativeCumquat.ts
+++ modules/cumquat-native/specs/NativeCumquat.ts
@@ -2,12 +2,18 @@ import type {TurboModule} from 'react-native';
 import {TurboModuleRegistry} from 'react-native';
 
 export interface Spec extends TurboModule {
-  createEngine(config: Object): number;
-  initialize(handle: number, pois: ReadonlyArray<Object>): void;
-  update(handle: number, sensorState: Object): number;
-  getFrame(handle: number): Object;
-  pick(handle: number, x: number, y: number, radiusPixels: number): Object | null;
-  destroyEngine(handle: number): void;
+  readonly getVersion: () => string;
+  readonly createEngine: (config: Object) => number;
+  readonly initialize: (handle: number, pois: ReadonlyArray<Object>) => void;
+  readonly update: (handle: number, sensorState: Object) => number;
+  readonly getFrame: (handle: number) => Object;
+  readonly pick: (
+    handle: number,
+    x: number,
+    y: number,
+    radiusPixels: number,
+  ) => Object | null;
+  readonly destroyEngine: (handle: number) => void;
 }
 
 export default TurboModuleRegistry.getEnforcing<Spec>('NativeCumquat');
