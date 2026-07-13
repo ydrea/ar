7c8440877379aefdb271fb453eaa3fdb7b00631d
Andrija Vranic <ydrea.wrld@gmail.com>
2026-07-13 09:08:58 +0200

    Add native setViewState TurboModule method

--- modules/cumquat-native/specs/NativeCumquat.ts
+++ modules/cumquat-native/specs/NativeCumquat.ts
@@ -5,6 +5,7 @@ export interface Spec extends TurboModule {
   readonly getVersion: () => string;
   readonly createEngine: (config: Object) => number;
   readonly initialize: (handle: number, pois: ReadonlyArray<Object>) => void;
+  readonly setViewState: (handle: number, viewState: Object) => void;
   readonly update: (handle: number, sensorState: Object) => number;
   readonly getFrame: (handle: number) => Object;
   readonly pick: (
