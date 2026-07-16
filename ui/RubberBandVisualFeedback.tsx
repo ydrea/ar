c332d9b05f3e682685b378cff42afe3e4cd90da9
Andrija Vranic <ydrea.wrld@gmail.com>
2026-07-16 19:05:52 +0200

    fix absolute fill typing on React Native 0.85

--- ui/RubberBandVisualFeedback.tsx
+++ ui/RubberBandVisualFeedback.tsx
@@ -65,7 +65,11 @@ export function RubberBandVisualFeedback({
 
 const styles = StyleSheet.create({
   container: {
-    ...StyleSheet.absoluteFillObject,
+    position: "absolute",
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
     overflow: "hidden",
   },
   edge: {
