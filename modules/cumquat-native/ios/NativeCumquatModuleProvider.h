a61a5f80bdf7e12d97514a78edc15ea83c7ea58a
Andrija Vranic <ydrea.wrld@gmail.com>
2026-07-20 10:20:25 +0200

    feat(ios): add Cumquat module provider header

---
+++ modules/cumquat-native/ios/NativeCumquatModuleProvider.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+#import <ReactCommon/RCTTurboModule.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NativeCumquatModuleProvider : NSObject <RCTModuleProvider>
+
+@end
+
+NS_ASSUME_NONNULL_END
